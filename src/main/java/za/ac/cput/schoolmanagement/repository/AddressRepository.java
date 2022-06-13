07e3e8bc4a78581cada0758c1f430259564ba5d9
Sinenhlanhla Zondi <sinezondi17@gmail.com>
2022-06-14 00:00:13 +0200

    completed Address and City repositories

--- src/main/java/za/ac/cput/schoolmanagement/repository/AddressRepository.java
+++ src/main/java/za/ac/cput/schoolmanagement/repository/AddressRepository.java
@@ -8,5 +8,10 @@ package za.ac.cput.schoolmanagement.repository;
  *
  * */
 
-public interface AddressRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import za.ac.cput.schoolmanagement.domain.Address;
+
+@Repository
+public interface AddressRepository extends JpaRepository<Address, String> {
 }
--- src/main/java/za/ac/cput/schoolmanagement/repository/CityRepository.java
+++ src/main/java/za/ac/cput/schoolmanagement/repository/CityRepository.java
@@ -1,4 +1,9 @@
 package za.ac.cput.schoolmanagement.repository;
 
-public interface CityRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import za.ac.cput.schoolmanagement.domain.City;
+
+@Repository
+public interface CityRepository extends JpaRepository<City, String> {
 }
