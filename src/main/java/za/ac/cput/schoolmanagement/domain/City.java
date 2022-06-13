ee2f95231c5efff9ffcd32d3e9643227292b0dcd
Sinenhlanhla Zondi <sinezondi17@gmail.com>
2022-06-13 23:09:27 +0200

    completed City model class

--- src/main/java/za/ac/cput/schoolmanagement/domain/City.java
+++ src/main/java/za/ac/cput/schoolmanagement/domain/City.java
@@ -8,5 +8,96 @@ package za.ac.cput.schoolmanagement.domain;
  *
  * */
 
-public class City {
+public class City
+{
+    private String id;
+    private String name;
+    private Country country;
+
+    public City(CityBuilder b)
+    {
+        this.id = b.id;
+        this.name = b.name;
+        this.country = b.country;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Country getCountry() {
+        return country;
+    }
+
+    @Override
+    public String toString() {
+        return "City{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if(this == obj)
+        {
+            return true;
+        }
+        if(obj == null || getClass() != obj.getClass())
+        {
+            return false;
+        }
+        if(!super.equals(obj))
+        {
+            return false;
+        }
+        City c = (City) obj;
+        return id.equals(c.id) &&
+                name.equals(c.name) /*&&
+                country.equals(c.country)*/;
+    }
+
+    public static class CityBuilder
+    {
+        private String id;
+        private String name;
+        private Country country;
+
+        public CityBuilder setCityId(String id)
+        {
+            this.id = id;
+            return this;
+        }
+
+        public CityBuilder setCityName(String name)
+        {
+            this.name = name;
+            return this;
+        }
+
+        public CityBuilder setCityCountry(Country country)
+        {
+            this.country = country;
+            return this;
+        }
+
+        public City.CityBuilder copy(City city)
+        {
+            this.id = city.id;
+            this.name = city.name;
+            this.country = city.country;
+            return this;
+        }
+
+        public City build()
+        {
+            City c = new City(this);
+            return c;
+        }
+    }
 }
