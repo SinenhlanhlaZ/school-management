95c4b51fbea3f065e5436f9390201916f925136b
Sinenhlanhla Zondi <sinezondi17@gmail.com>
2022-06-13 23:56:14 +0200

    corrected CityRepo to interface file

--- src/main/java/za/ac/cput/schoolmanagement/repository/CityRepository.java
+++ src/main/java/za/ac/cput/schoolmanagement/repository/CityRepository.java
@@ -1,12 +1,4 @@
 package za.ac.cput.schoolmanagement.repository;
 
-/*
- *
- * Sinenhlanhla Zondi
- * 220008922
- * 12/06/20220
- *
- * */
-
-public class CityRepository {
+public interface CityRepository {
 }
