384e1ea41806ac124972fbeda8dffbcfbb96fc29
Shina-Kara <219333181@mycput.ac.za>
2022-06-14 01:07:38 +0200

    Student Repository Entity

---
+++ src/main/java/za/ac/cput/schoolmanagement/repository/StudentRepository.java
@@ -0,0 +1,13 @@
+package za.ac.cput.schoolmanagement.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+/*
+    Student.java
+    Repository for Student
+    Author: Shina Kara 219333181
+    Date: 12/06/2022
+*/
+@Repository
+public interface StudentRepository extends JpaRepository<StudentRepository, String> {
+}
