c8e1232f05b2ec060e464b890d57d0d7614e26ee
Sinenhlanhla Zondi <sinezondi17@gmail.com>
2022-06-16 21:07:53 +0200

    completed cityService

--- src/main/java/za/ac/cput/schoolmanagement/service/ICityService.java
+++ src/main/java/za/ac/cput/schoolmanagement/service/ICityService.java
@@ -1,4 +1,11 @@
 package za.ac.cput.schoolmanagement.service;
 
-public interface ICityService {
+import za.ac.cput.schoolmanagement.domain.City;
+
+import java.util.List;
+
+public interface ICityService extends IService<City, String>{
+    List<City> findAll();
+
+    public void deleteById(String id);
 }
