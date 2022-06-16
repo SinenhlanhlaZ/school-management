a9f0dcee695988bcf241a787f19c17a538b366e4
Sinenhlanhla Zondi <sinezondi17@gmail.com>
2022-06-16 23:27:05 +0200

    created test data and methods

--- src/test/java/za/ac/cput/schoolmanagement/service/impl/AddressServiceImplTest.java
+++ src/test/java/za/ac/cput/schoolmanagement/service/impl/AddressServiceImplTest.java
@@ -1,21 +1,45 @@
 package za.ac.cput.schoolmanagement.service.impl;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import za.ac.cput.schoolmanagement.domain.Address;
-import za.ac.cput.schoolmanagement.domain.City;
-import za.ac.cput.schoolmanagement.domain.Employee;
-import za.ac.cput.schoolmanagement.domain.Name;
+import za.ac.cput.schoolmanagement.domain.*;
 import za.ac.cput.schoolmanagement.factory.AddressFactory;
 import za.ac.cput.schoolmanagement.factory.CityFactory;
 import za.ac.cput.schoolmanagement.factory.EmployeeFactory;
 import za.ac.cput.schoolmanagement.factory.NameFactory;
+import za.ac.cput.schoolmanagement.service.IAddressService;
 
 @SpringBootTest
 public class AddressServiceImplTest {
 
-    //private final Name name = NameFactory.build("Fayaad", "middle", "Abrahams");
-    //private final Employee employee = EmployeeFactory.build("2323123131", "eazypeazy@cput.ac.za", name);
-
     Address addr = AddressFactory.build("5", "Ninth Avenue", "91", "", 4001, new City());
-    City city = CityFactory.build("1", "Durban");
+
+    @Autowired
+    IAddressService addrService;
+
+    @Test
+    public void save()
+    {
+
+    }
+
+    @Test
+    public void read()
+    {
+
+    }
+
+    @Test
+    public void delete()
+    {
+
+    }
+
+    @Test
+    public void readAll()
+    {
+
+    }
+
 }
--- src/test/java/za/ac/cput/schoolmanagement/service/impl/CityServiceImplTest.java
+++ src/test/java/za/ac/cput/schoolmanagement/service/impl/CityServiceImplTest.java
@@ -1,4 +1,36 @@
 package za.ac.cput.schoolmanagement.service.impl;
 
+import org.junit.jupiter.api.Test;
+import za.ac.cput.schoolmanagement.domain.City;
+import za.ac.cput.schoolmanagement.domain.Country;
+import za.ac.cput.schoolmanagement.factory.CityFactory;
+
 public class CityServiceImplTest {
+
+    City city = CityFactory.build("1", "Durban", new Country());
+    
+    @Test
+    public void save()
+    {
+
+    }
+
+    @Test
+    public void read()
+    {
+
+    }
+
+    @Test
+    public void delete()
+    {
+
+    }
+
+    @Test
+    public void readAll()
+    {
+
+    }
+
 }
