ee00db153bd21e1619d0abfe00894f5127c51cdd
Sinenhlanhla Zondi <sinezondi17@gmail.com>
2022-06-17 18:28:42 +0200

    completed cityServiceImplTest

--- src/test/java/za/ac/cput/schoolmanagement/service/impl/CityServiceImplTest.java
+++ src/test/java/za/ac/cput/schoolmanagement/service/impl/CityServiceImplTest.java
@@ -5,13 +5,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import za.ac.cput.schoolmanagement.domain.City;
 import za.ac.cput.schoolmanagement.domain.Country;
+import za.ac.cput.schoolmanagement.domain.Employee;
 import za.ac.cput.schoolmanagement.factory.CityFactory;
 import za.ac.cput.schoolmanagement.service.ICityService;
 
 import java.util.List;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 public class CityServiceImplTest {
@@ -33,13 +35,19 @@ public class CityServiceImplTest {
     @Test
     public void read()
     {
-
+        Optional<City> read = this.cityService.read(this.city.getId());
+        assertAll(
+                () -> assertTrue(read.isPresent()),
+                () -> assertEquals(this.city, read.get())
+        );
     }
 
     @Test
     public void delete()
     {
-
+        this.cityService.delete(this.city);
+        List<City> listOfCities = this.cityService.findAll();
+        assertEquals(0, listOfCities.size());
     }
 
     @Test
