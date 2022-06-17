4912c59c83014ae055bbad63306d72ae6a712379
Sinenhlanhla Zondi <sinezondi17@gmail.com>
2022-06-17 20:24:57 +0200

    completed controller stuff

--- src/main/java/za/ac/cput/schoolmanagement/controller/CityController.java
+++ src/main/java/za/ac/cput/schoolmanagement/controller/CityController.java
@@ -8,5 +8,58 @@ package za.ac.cput.schoolmanagement.controller;
  *
  * */
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import za.ac.cput.schoolmanagement.domain.City;
+import za.ac.cput.schoolmanagement.service.ICityService;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("schoolmanagement/city/")
+@Slf4j
 public class CityController {
+    private ICityService cityService;
+
+    @Autowired
+    CityController(ICityService cityService)
+    {
+        this.cityService = cityService;
+    }
+
+    @PostMapping
+    public ResponseEntity<City> save(@Valid @RequestBody City city)
+    {
+        log.info("Save city: {}", city);
+        City saveCity = cityService.save(city);
+        return ResponseEntity.ok(saveCity);
+    }
+
+    @GetMapping("read/{id}")
+    public ResponseEntity<City> read(@PathVariable String id)
+    {
+        log.info("Read city: {}", id);
+        City readCity = this.cityService.read(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        return ResponseEntity.ok(readCity);
+    }
+
+    @DeleteMapping("delete/{id}")
+    public ResponseEntity<Void> delete(@PathVariable String id)
+    {
+        log.info("delete city: {}", id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("all")
+    public ResponseEntity<List<City>> findAll()
+    {
+        List<City> allCities = this.cityService.findAll();
+        return ResponseEntity.ok(allCities);
+    }
+
 }
--- src/test/java/za/ac/cput/schoolmanagement/controller/CityControllerTest.java
+++ src/test/java/za/ac/cput/schoolmanagement/controller/CityControllerTest.java
@@ -1,4 +1,85 @@
 package za.ac.cput.schoolmanagement.controller;
 
+/*
+ *
+ * Sinenhlanhla Zondi
+ * 220008922
+ * 12/06/20220
+ *
+ * */
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import za.ac.cput.schoolmanagement.domain.City;
+import za.ac.cput.schoolmanagement.domain.Country;
+import za.ac.cput.schoolmanagement.domain.Employee;
+import za.ac.cput.schoolmanagement.factory.CityFactory;
+import za.ac.cput.schoolmanagement.factory.CountryFactory;
+import za.ac.cput.schoolmanagement.factory.EmployeeFactory;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CityControllerTest {
+
+    Country country = CountryFactory.build("1", "South Africa");
+
+    @LocalServerPort
+    private int portNum;
+
+    private City city;
+    private String url;
+
+    @Autowired
+    private TestRestTemplate restTmplte;
+
+    @Autowired
+    private CityController cityCtrl;
+
+    @BeforeEach
+    public void setUp() {
+        this.city = CityFactory.build("1", "Durban", new Country());
+        this.url = "http://localhost:" + this.portNum + "/schoolmanagement/city/";
+    }
+
+    @Test
+    public void read()
+    {
+        String strUrl = url + "read/" + this.city.getId();
+        ResponseEntity<City> response = this.restTmplte.getForEntity(strUrl, City.class);
+
+        assertAll(() -> assertEquals(HttpStatus.OK, response.getStatusCode()), () -> assertNotNull(response.getBody()));
+    }
+
+    @Test
+    public void save()
+    {
+        String strUrl = url + "save";
+        ResponseEntity<City> response = this.restTmplte.postForEntity(strUrl, this.city, City.class);
+
+        assertAll(() -> assertEquals(HttpStatus.OK, response.getStatusCode()), () -> assertNotNull(response.getBody()));
+    }
+
+
+    @Test
+    public void delete()
+    {
+        String strUrl = url + "delete/" + this.city.getId();
+        this.restTmplte.delete(strUrl);
+    }
+
+    @Test
+    public void findAll()
+    {
+        String strUrl = url + "all";
+        ResponseEntity<City[]> response = this.restTmplte.getForEntity(strUrl, City[].class);
+        assertAll(() -> assertEquals(HttpStatus.OK, response.getStatusCode()), () -> assertTrue(response.getBody().length == 0));
+    }
+
 }
