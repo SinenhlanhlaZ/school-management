10faf5cdc804f44a072f70df1d24e28f8e8d6646
Sinenhlanhla Zondi <sinezondi17@gmail.com>
2022-06-17 23:29:02 +0200

    commit

--- src/test/java/za/ac/cput/schoolmanagement/service/impl/CityServiceImplTest.java
+++ src/test/java/za/ac/cput/schoolmanagement/service/impl/CityServiceImplTest.java
@@ -5,10 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import za.ac.cput.schoolmanagement.domain.City;
 import za.ac.cput.schoolmanagement.domain.Country;
+import za.ac.cput.schoolmanagement.domain.Employee;
 import za.ac.cput.schoolmanagement.factory.CityFactory;
 import za.ac.cput.schoolmanagement.service.ICityService;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertSame;
@@ -31,12 +33,15 @@ public class CityServiceImplTest {
 
     @Test
     public void read() {
-
+        Optional<City> read = this.cityService.read(this.city.getId());
+        assertAll(() -> assertTrue(read.isPresent()), () -> assertEquals(this.city, read.get()));
     }
 
     @Test
     public void delete() {
-
+        this.cityService.delete(this.city);
+        List<City> cityList = this.cityService.findAll();
+        assertEquals(0, cityList.size());
     }
 
     @Test
