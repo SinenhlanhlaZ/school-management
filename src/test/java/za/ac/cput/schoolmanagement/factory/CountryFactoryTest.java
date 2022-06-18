67981c59fa31e46fe159a69f751fbf46500752b1
NonzwakaziMgxaji <nonzwakazimgxaji@gmail.com>
2022-06-18 23:19:30 +0200

    fixed country factory

--- src/test/java/za/ac/cput/schoolmanagement/factory/CountryFactoryTest.java
+++ src/test/java/za/ac/cput/schoolmanagement/factory/CountryFactoryTest.java
@@ -1,6 +1,5 @@
 package za.ac.cput.schoolmanagement.factory;
 
-<<<<<<< HEAD
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import za.ac.cput.schoolmanagement.domain.Country;
@@ -11,9 +10,11 @@ Country Factory Test
 Nonzwakazi Mgxaji 213181584
  */
 
-=======
->>>>>>> 3d8b6c6026a71e6d8102d046864ff8ad0f0c63bd
 class CountryFactoryTest {
+    @Test
+    void buildWithId(){
+        Country country = new Country();
+        country.setId("29");
+        Assertions.assertNotNull(country.getId());
+    }
 }
-
-//still working on it
